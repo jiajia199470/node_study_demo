25686a7f65e60cb5b3359b3cc6035145e71504d0
jiajia199470 <1083573413@qq.com>
2018-04-10 18:42:33 +0800

    init

---
+++ server/routes/goods.js
@@ -0,0 +1,155 @@
+/**
+ * Created by Administrator on 2018/4/10.
+ */
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var Goods = require('../models/goods');
+
+//链接MongoDB数据库
+var options = {
+    db: { native_parser: true },
+    server: { poolSize: 5 },
+    user: 'root',
+    pass: '123456'
+};
+
+var url = 'mongodb://127.0.0.1:28017/imoocmall';//数据库地址
+
+mongoose.connect(url,options);
+
+mongoose.connection.on("connected", function () {
+    console.log("MongoDB connected success.")
+});
+
+mongoose.connection.on("error", function () {
+    console.log("MongoDB connected fail.")
+});
+
+mongoose.connection.on("disconnected", function () {
+    console.log("MongoDB connected disconnected.")
+});
+//查询商品列表数据
+router.get("/list", function (req,res,next) {
+    let page = parseInt(req.param("page"));
+    let pageSize = parseInt(req.param("pageSize"));
+    let priceLevel = req.param("priceLevel");
+    let sort = req.param("sort");
+    let skip = (page-1)*pageSize;
+    var priceGt = '',priceLte = '';
+    let params = {};
+    if(priceLevel!='all'){
+        switch (priceLevel){
+            case '0':priceGt = 0;priceLte=100;break;
+            case '1':priceGt = 100;priceLte=500;break;
+            case '2':priceGt = 500;priceLte=1000;break;
+            case '3':priceGt = 1000;priceLte=5000;break;
+        }
+        params = {
+            salePrice:{
+                $gt:priceGt,
+                $lte:priceLte
+            }
+        }
+    }
+    let goodsModel = Goods.find(params).skip(skip).limit(pageSize);
+    goodsModel.sort({'salePrice':sort});
+    goodsModel.exec(function (err,doc) {
+        if(err){
+            res.json({
+                status:'1',
+                msg:err.message
+            });
+        }else{
+            res.json({
+                status:'0',
+                msg:'',
+                result:{
+                    count:doc.length,
+                    list:doc
+                }
+            });
+        }
+    })
+});
+
+/*提交数据，加入购物车*/
+router.post('/addCart', function(req, res, next) {
+    var userId = '100000077',
+        productId = req.body.productId;
+    var User = require('../models/user');
+    User.findOne({userId:userId},function (err,userDoc) {
+        if(err){
+            res.json({
+                status:"1",
+                msg:err.message
+            });
+        }else {
+            // console.log('userDoc:'+userDoc);
+            if(userDoc){
+                Goods.findOne({productId:productId},function (goodsErr,goodsDoc) {
+                    console.log('goodsDoc：'+goodsDoc);
+                    if (goodsErr){
+                        res.json({
+                            status:"1",
+                            msg:goodsErr.message
+                        })
+                    }else {
+                        if(goodsDoc){
+                            let goodsItem ='';
+                            //遍历，用户是否已有商品
+                            userDoc.cartList.forEach(function (item) {
+                                if(item.productId == productId){
+                                    goodsItem = item;
+                                    item.productNum ++;
+                                }
+                            });
+
+                            //判断用户是否已有商品
+                            if(goodsItem){//已有商品
+
+                                userDoc.save(function (saveErr, saveDoc) {
+                                    if (saveErr) {
+                                        res.json({
+                                            status: "1",
+                                            msg: saveErr.message
+                                        })
+                                    } else {
+                                        res.json({
+                                            status: "0",
+                                            msg: "",
+                                            result: 'success'
+                                        })
+                                    }
+                                })
+
+                            }else {//插入新纪录
+
+                                goodsDoc.productNum = 1;
+                                goodsDoc.checked = 1;
+                                userDoc.cartList.push(goodsDoc);
+                                userDoc.save(function (saveErr, saveDoc) {
+                                    if (saveErr) {
+                                        res.json({
+                                            status: "1",
+                                            msg: saveErr.message
+                                        })
+                                    } else {
+                                        res.json({
+                                            status: "0",
+                                            msg: "",
+                                            result: 'success'
+                                        })
+                                    }
+                                })
+
+                            }
+                        }
+                    }
+                });
+            }
+        }
+    });
+});
+
+module.exports = router;
---
+++ src/views/Image.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>我是商品图片
+    </div>
+
+</template>
+
+<script type="text/ecmascript-6">
+
+    export default{
+        data(){
+            return{
+                msg:"111"
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
